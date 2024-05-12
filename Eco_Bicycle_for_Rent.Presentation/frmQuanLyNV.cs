dd7b8095e2284c31842561a83b2be48e61d434fa
Cwuzto <phucminh571@gmail.com>
2024-05-12 18:49:35 +0700

    3 form from tgb

--- Eco_Bicycle_for_Rent.Presentation/frmQuanLyNV.cs
+++ Eco_Bicycle_for_Rent.Presentation/frmQuanLyNV.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Eco_Bicycle_for_Rent.Presentation
 {
-    public partial class Form1 : Form
+    public partial class frmQuanLyNV : Form
     {
-        public Form1()
+        public frmQuanLyNV()
         {
             InitializeComponent();
         }
